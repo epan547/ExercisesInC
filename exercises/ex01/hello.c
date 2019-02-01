2e235e0a9c388cdb5f541e943951e50bc1d86804
Emma Pan <epan547@gmail.com>
2019-02-01 12:29:02 -0500

    optimization

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5; // with flag -o2, code still acts normal
+    int y = x + 1;
+
+    printf("%d\n", y);
     return 0;
 }
+
+
+/*
+  2) code executed as normal with -o2 flag
+  3) printing x works as expected with and without optimization.
+  However, the assembly code is shorter, so it probabaly has a shorter run time
+  4) printing y works as expected with and without optimization.
+  However, the assembly code is shorter, so it probably has a shorter run time.
+*/
