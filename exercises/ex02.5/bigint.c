ee58bda55706fa2060b635233247d8c5e4be59b8
Emma Pan <epan547@gmail.com>
2019-02-22 02:14:20 -0500

    HW 03 complete

--- exercises/ex02.5/bigint.c
+++ exercises/ex02.5/bigint.c
@@ -27,256 +27,256 @@ Follow these steps to get this program working:
 
 /* reverse_string: Returns a new string with the characters reversed.
 
-It is the caller's responsibility to free the result.
+   It is the caller's responsibility to free the result.
 
-s: string
-returns: string
-*/
+   s: string
+   returns: string
+ */
 
 
 char *reverse_string(char *s) {
 // Initializing string length
-    size_t len = strlen(s);
-  // Creating aray to hold answer.
-    char* reverse = malloc(len);
-    // printf("%li \n", len);
-    char* t = s + len-1;
-    // printf("%p, %p \n", t, s);
-    while(t >= s){
-      strncat(reverse, t, 1);
-      t = t - 1;
-    }
-    s = reverse;
-    // printf("%s \n", s);
-    return s;
+        size_t len = strlen(s);
+        // Creating aray to hold answer.
+        char* reverse = malloc(len);
+        // printf("%li \n", len);
+        char* t = s + len-1;
+        // printf("%p, %p \n", t, s);
+        while(t >= s) {
+                strncat(reverse, t, 1);
+                t = t - 1;
+        }
+        s = reverse;
+        // printf("%s \n", s);
+        return s;
 }
 
 /* ctoi: Converts a character to integer.
 
-c: one of the characters '0' to '9'
-returns: integer 0 to 9
-*/
+   c: one of the characters '0' to '9'
+   returns: integer 0 to 9
+ */
 int ctoi(char c) {
-    assert(isdigit(c));
-    return c - '0';
+        assert(isdigit(c));
+        return c - '0';
 }
 
 /* itoc: Converts an integer to character.
 
-i: integer 0 to 9
-returns: character '0' to '9'
-*/
+   i: integer 0 to 9
+   returns: character '0' to '9'
+ */
 char itoc(int i) {
-    assert(i >= 0 && i < 10);
-    return '0' + i;
+        assert(i >= 0 && i < 10);
+        return '0' + i;
 }
 
 /* add_digits: Adds two decimal digits, returns the total and carry.
 
-For example, if a='5', b='6', and carry='1', the sum is 12, so
-the output value of total should be '2' and carry should be '1'
+   For example, if a='5', b='6', and carry='1', the sum is 12, so
+   the output value of total should be '2' and carry should be '1'
 
-a: character '0' to '9'
-b: character '0' to '9'
-c: character '0' to '9'
-total: pointer to char
-carry: pointer to char
+   a: character '0' to '9'
+   b: character '0' to '9'
+   c: character '0' to '9'
+   total: pointer to char
+   carry: pointer to char
 
-*/
+ */
 void add_digits(char a, char b, char c, char *total, char *carry) {
-    int sum = ctoi(a) + ctoi(b) + ctoi(c);
-    if(sum > 19){
-      *carry = itoc(2);
-      *total = itoc(sum-20);
-    }
-    else if(sum > 9) {
-      *carry = itoc(1);
-      *total = itoc(sum-10);
-    }
-    else{
-      *carry = itoc(0);
-      *total = itoc(sum);
-    }
-    return;
+        int sum = ctoi(a) + ctoi(b) + ctoi(c);
+        if(sum > 19) {
+                *carry = itoc(2);
+                *total = itoc(sum-20);
+        }
+        else if(sum > 9) {
+                *carry = itoc(1);
+                *total = itoc(sum-10);
+        }
+        else{
+                *carry = itoc(0);
+                *total = itoc(sum);
+        }
+        return;
 }
 
 /* Define a type to represent a BigInt.
    Internally, a BigInt is a string of digits, with the digits in
    reverse order.
-*/
+ */
 typedef char * BigInt;
 
 /* add_bigint: Adds two BigInts
 
-Stores the result in z.
+   Stores the result in z.
 
-x: BigInt
-y: BigInt
-carry_in: char
-z: empty buffer
-*/
+   x: BigInt
+   y: BigInt
+   carry_in: char
+   z: empty buffer
+ */
 void add_bigint(BigInt x, BigInt y, char carry_in, BigInt z) {
-    char total, carry_out;
-    int dx=1, dy=1, dz=1;
-    char a, b;
+        char total, carry_out;
+        int dx=1, dy=1, dz=1;
+        char a, b;
 
-    /* OPTIONAL TODO: Modify this function to allocate and return z
-    *  rather than taking an empty buffer as a parameter.
-    *  Hint: you might need a helper function.
-    */
+        /* OPTIONAL TODO: Modify this function to allocate and return z
+         *  rather than taking an empty buffer as a parameter.
+         *  Hint: you might need a helper function.
+         */
 
-    if (*x == '\0') {
-        a = '0';
-        dx = 0;
-    }else{
-        a = *x;
-    }
-    if (*y == '\0') {
-        b = '0';
-        dy = 0;
-    }else{
-        b = *y;
-    }
+        if (*x == '\0') {
+                a = '0';
+                dx = 0;
+        }else{
+                a = *x;
+        }
+        if (*y == '\0') {
+                b = '0';
+                dy = 0;
+        }else{
+                b = *y;
+        }
 
-    // printf("%c %c %c\n", a, b, carry_in);
-    add_digits(a, b, carry_in, &total, &carry_out);
-    // printf("%c %c\n", carry_out, total);
+        // printf("%c %c %c\n", a, b, carry_in);
+        add_digits(a, b, carry_in, &total, &carry_out);
+        // printf("%c %c\n", carry_out, total);
 
-    // if total and carry are 0, we're done
-    if (total == '0' && carry_out == '0') {
-        *z = '\0';
-        return;
-    }
-    // otherwise store the digit we just computed
-    *z = total;
+        // if total and carry are 0, we're done
+        if (total == '0' && carry_out == '0') {
+                *z = '\0';
+                return;
+        }
+        // otherwise store the digit we just computed
+        *z = total;
 
-    // and make a recursive call to fill in the rest.
-    add_bigint(x+dx, y+dy, carry_out, z+dz);
+        // and make a recursive call to fill in the rest.
+        add_bigint(x+dx, y+dy, carry_out, z+dz);
 }
 
 /* print_bigint: Prints the digits of BigInt in the normal order.
 
-big: BigInt
-*/
+   big: BigInt
+ */
 void print_bigint(BigInt big) {
-    char c = *big;
-    if (c == '\0') return;
-    print_bigint(big+1);
-    printf("%c", c);
+        char c = *big;
+        if (c == '\0') return;
+        print_bigint(big+1);
+        printf("%c", c);
 }
 
 /* make_bigint: Creates and returns a BigInt.
 
-Caller is responsible for freeing.
+   Caller is responsible for freeing.
 
-s: string of digits in the usual order
-returns: BigInt
-*/
+   s: string of digits in the usual order
+   returns: BigInt
+ */
 BigInt make_bigint(char *s) {
-    char *r = reverse_string(s);
-    return (BigInt) r;
+        char *r = reverse_string(s);
+        return (BigInt) r;
 }
 
 void test_reverse_string() {
 
-    char* s = "000000000000000000000000000000000000000000001";
-    // printf("%s \n", s);
-    char* t = reverse_string(s);
-    if (strcmp(t, "100000000000000000000000000000000000000000000") == 0) {
-        printf("reverse_string passed\n");
-    } else {
-        printf("reverse_string failed\n");
-    }
+        char* s = "000000000000000000000000000000000000000000001";
+        // printf("%s \n", s);
+        char* t = reverse_string(s);
+        if (strcmp(t, "100000000000000000000000000000000000000000000") == 0) {
+                printf("reverse_string passed\n");
+        } else {
+                printf("reverse_string failed\n");
+        }
 
-    //   s = "000000000000000000000000000000000000000000001";
-    // printf("%s \n", s);
-    // t = reverse_string(s);
-    // if (strcmp(t, "100000000000000000000000000000000000000000000") == 0) {
-    //     printf("reverse_string passed\n");
-    // } else {
-    //     printf("reverse_string failed\n");
-    // }  char *s = "123";
-    // char *t = reverse_string(s);
-    // if (strcmp(t, "321") == 0) {
-    //     printf("reverse_string passed\n");
-    // } else {
-    //     printf("reverse_string failed\n");
-    // }
-    //
-    // // Adding additional tests
-    // s = "99999999999999999999999999999999999999999999";
-    // printf("%s\n", s);
-    // t = reverse_string(s);
-    // if (strcmp(t, "99999999999999999999999999999999999999999999") == 0) {
-    //     printf("reverse_string passed\n");
-    // } else {
-    //     printf("reverse_string failed\n");
-    // }
+        //   s = "000000000000000000000000000000000000000000001";
+        // printf("%s \n", s);
+        // t = reverse_string(s);
+        // if (strcmp(t, "100000000000000000000000000000000000000000000") == 0) {
+        //     printf("reverse_string passed\n");
+        // } else {
+        //     printf("reverse_string failed\n");
+        // }  char *s = "123";
+        // char *t = reverse_string(s);
+        // if (strcmp(t, "321") == 0) {
+        //     printf("reverse_string passed\n");
+        // } else {
+        //     printf("reverse_string failed\n");
+        // }
+        //
+        // // Adding additional tests
+        // s = "99999999999999999999999999999999999999999999";
+        // printf("%s\n", s);
+        // t = reverse_string(s);
+        // if (strcmp(t, "99999999999999999999999999999999999999999999") == 0) {
+        //     printf("reverse_string passed\n");
+        // } else {
+        //     printf("reverse_string failed\n");
+        // }
 
 
 
 }
 
 void test_itoc() {
-    char c = itoc(3);
-    if (c == '3') {
-        printf("itoc passed\n");
-    } else {
-        printf("itoc failed\n");
-    }
+        char c = itoc(3);
+        if (c == '3') {
+                printf("itoc passed\n");
+        } else {
+                printf("itoc failed\n");
+        }
 }
 
 void test_add_digits() {
-    char total, carry;
-    add_digits('7', '4', '1', &total, &carry);
-    if (total == '2' && carry == '1') {
-        printf("add_digits passed\n");
-    } else {
-        printf("add_digits failed\n");
-    }
+        char total, carry;
+        add_digits('7', '4', '1', &total, &carry);
+        if (total == '2' && carry == '1') {
+                printf("add_digits passed\n");
+        } else {
+                printf("add_digits failed\n");
+        }
 
-    // Adding another test
-    add_digits('9', '9', '9', &total, &carry);
-    if (total == '7' && carry == '2') {
-        printf("add_digits passed\n");
-    } else {
-        printf("add_digits failed\n");
-    }
+        // Adding another test
+        add_digits('9', '9', '9', &total, &carry);
+        if (total == '7' && carry == '2') {
+                printf("add_digits passed\n");
+        } else {
+                printf("add_digits failed\n");
+        }
 
-    add_digits('0', '0', '0', &total, &carry);
-    if (total == '0' && carry == '0') {
-        printf("add_digits passed\n");
-    } else {
-        printf("add_digits failed\n");
-    }
+        add_digits('0', '0', '0', &total, &carry);
+        if (total == '0' && carry == '0') {
+                printf("add_digits passed\n");
+        } else {
+                printf("add_digits failed\n");
+        }
 }
 
 void test_add_bigint() {
-    char *s = "1";
-    char *t = "99999999999999999999999999999999999999999999";
-    char *res = "000000000000000000000000000000000000000000001";
+        char *s = "1";
+        char *t = "99999999999999999999999999999999999999999999";
+        char *res = "000000000000000000000000000000000000000000001";
 
-    BigInt big1 = make_bigint(s);
-    BigInt big2 = make_bigint(t);
-    BigInt big3 = malloc(100);
+        BigInt big1 = make_bigint(s);
+        BigInt big2 = make_bigint(t);
+        BigInt big3 = malloc(100);
 
-  	add_bigint(big1, big2, '0', big3);
-    // printf("here %s \n", big3);
+        add_bigint(big1, big2, '0', big3);
+        // printf("here %s \n", big3);
 
-    if (strcmp(big3, res) == 0) {
-        printf("add_bigint passed\n");
-    } else {
-        printf("add_bigint failed\n");
-    }
+        if (strcmp(big3, res) == 0) {
+                printf("add_bigint passed\n");
+        } else {
+                printf("add_bigint failed\n");
+        }
 }
 
 int main (int argc, char *argv[])
 {
-    test_reverse_string();
-    test_itoc();
-    test_add_digits();
+        test_reverse_string();
+        test_itoc();
+        test_add_digits();
 
-    //      uncomment the following, and it should work.
-    test_add_bigint();
-    return 0;
+        //      uncomment the following, and it should work.
+        test_add_bigint();
+        return 0;
 }
---
+++ exercises/ex03/tee.c
@@ -0,0 +1,79 @@
+/* This is a rudimentary C implementation of the tee terminal command, linked here: http://man7.org/linux/man-pages/man1/tee.1.html
+
+The program reads user input, assuming the first argument is "tee", and then
+echoes user input, writing it to a filename, specified in the last argument.
+
+If the -a flag is given, then the user input will be appended to the file,
+instead of re-writing it, if it already exists (default).
+
+
+TODO:
+    - Add error catching
+    - Add additional functionality
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define MAX 80
+
+
+int main(int argc, char*argv[]){
+  char ch;
+  char* filename;
+
+// Check for filename after tee command
+  if(argc > 1){
+    filename = argv[argc-1];
+    // printf("%s\n", filename);
+  }
+  else{
+    // Not an input, return
+    fprintf(stderr, "No filename");
+    return 0;
+  }
+
+// Flags for ways to change input
+  int a = 0;
+  int p = 0;
+  int i = 0;
+
+// Respond based on argument received
+  while((ch = getopt(argc, argv, "tee: a p i")) != EOF) {
+    switch(ch){
+      case 'p':
+        p = 1;
+        break;
+      case 'a':
+        a = 1;
+        break;
+      case 'i':
+        i = 1;
+        break;
+      default:
+        return 1;
+    }
+  }
+
+// Creating a file for writing
+FILE* file1;
+  if(a == 1){
+    file1 = fopen(filename, "a");
+  }
+  else {
+    file1 = fopen(filename, "w");
+  }
+
+
+  // Echoing the user input
+  char buff[MAX];
+  while(fgets(buff, MAX, stdin) != NULL){
+    printf("%s", buff);
+    fprintf(file1, "%s", buff);
+  }
+
+  fclose(file1);
+
+  return 0;
+}
